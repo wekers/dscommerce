73afd092a5cdd157405d75e5222ff4d8abc0ed48
Fernando Gilli <fernando@wekers.org>
2023-05-23 18:18:44 -0300

    change column name user_id to client_id

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -20,7 +20,7 @@ public class Order {
     private OrderStatus status;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "client_id")
     private User client;
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
