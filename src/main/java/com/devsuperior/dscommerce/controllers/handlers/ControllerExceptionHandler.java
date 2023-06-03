588da5e4755bc5fd2a8f8183a9972b0279316ea5
Fernando Gilli <fernando@wekers.org>
2023-06-03 16:52:01 -0300

    Bean Validation

--- src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.controllers.handlers;
 
 import com.devsuperior.dscommerce.dto.CustomError;
+import com.devsuperior.dscommerce.dto.ValidationError;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +28,18 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> MethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+          err.addError(f.getField(), f.getDefaultMessage());
+        }
+        //err.addError("name", "Mensagem de testes");
+        //err.addError("price", "preço invalido");
         return ResponseEntity.status(status).body(err);
     }
 
