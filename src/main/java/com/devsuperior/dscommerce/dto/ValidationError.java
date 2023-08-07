7e0652dc9deed406bec1e3fd2620c6cbf3bcfe79
Fernando Gilli <fernando@wekers.org>
2023-08-07 16:37:23 -0300

    Fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
@@ -17,6 +17,7 @@ public class ValidationError extends CustomError {
     }
 
     public void addError(String fieldName, String message){
+        errors.removeIf(x -> x.getFieldName().equals(fieldName)); //remove mensagem duplicada
         errors.add(new FieldMessage(fieldName, message));
     }
 }
